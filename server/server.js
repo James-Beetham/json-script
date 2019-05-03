cbe2577255954af4a4b7aa1cdc77a23e23194096
Atharva Kench <atharva.kench@gmail.com>
2019-05-03 11:09:05 -0400

    basic istyping feature working

--- server/server.js
+++ server/server.js
@@ -11,18 +11,43 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/templates/chatpage.html');
 })
 
+
+// list of all messages
+// a message contains a user and the text
+var allMessages = [];
+
+
+// when a user connects,
+// give them all the previous messages
 io.on('connection', (socket) => {
     console.log('user connected');
+    socket.emit('prevMessages', allMessages);
 
+    // when a new chat message is received
+    // store it 
+    // emit it to all users
     socket.on('chatmsg', (payload) => {
+        allMessages.push(payload);
         io.emit('chatmsg', payload);
     })
 
+    // when a user is typing
+    // emit it to all users except current user
+    socket.on('istyping', (user) => {
+        io.emit('istyping', user);
+    })
+
+    socket.on('stoppedtyping', (user) => {
+        io.emit('stoppedtyping', user);
+    })
+
+    // when someone logs off
     socket.on('disconnect', () => {
-        console.log('someone disconnected');
+        console.log('someone has disconnected');
     })
 })
 
+// listen on port 8000
 http.listen(8000, () => {
     console.log('server listening');
 })
--- server/static/chatpage.js
+++ server/static/chatpage.js
@@ -1,32 +1,97 @@
-var socket = io();
-
-socket.on('chatmsg', (payload) => {
-    var text = payload['username'] + ': ' + payload['message'];
-    $('#messages').append($('<li>').text(text));
-})
-
 // global variable representing user who is logged in 
 var username;
+// socket connected to server
+var socket = io();
+
+
+$(function () {
+
+    // send message on submit form
+    $('form').submit(function (e) {
+        e.preventDefault(); // prevents page reloading
+        var msg = document.getElementById('chatbox').value;
+        var payload = {
+            'username': username,
+            'message': msg
+        }
+        socket.emit('chatmsg', payload);
+        document.getElementById('chatbox').value = "";
+        return false;
+    });
+
+
+    var typing = false;
+    var timeout = undefined;
+
+    // called by timeout function
+    function hasStoppedTyping() {
+        typing = false;
+        socket.emit('stoppedtyping', username);
+    }
+
+    // send an istyping message if input is changing
+    $('#chatbox').on('change keyup paste', () => {
+        
+        // if the box is empty (user clicked enter recently), then return
+        if($('#chatbox').val() === '')
+            return;
+
+        // if was not typing, emit typing message to socket
+        if(!typing) {
+            typing = true;
+            socket.emit('istyping', username);
+            timeout = setTimeout(hasStoppedTyping, 1000);
+        } 
+        // if user is still typing, reset the timeout
+        else {
+            clearTimeout(timeout);
+            timeout = setTimeout(hasStoppedTyping, 1000);;
+        }
+    })
+});
+
+socket.on('chatmsg', (payload) => {
+    appendMessage(payload);
+});
+
+
+socket.on('istyping', (username) => {
+    $('#typing_users').append(buildTypingListElement(username));
+});
+
+socket.on('stoppedtyping', (username) => {
+    var id = getTypingId(username);
+    $(`#${id}`).remove();
+});
+
+socket.on('prevMessages', (prevMessages) => {
+    prevMessages.forEach(msg => {
+        appendMessage(msg);
+    });
+});
+
 
 function setUsername() {
 
     // get the username
     username = document.getElementById('username_input').value;
-    console.log(username);
 
     // make username input screen invisible and show the chat page
     document.getElementById('entry_div').style.display = 'none';
     document.getElementById('chatpage_div').style.display = 'block';
 }
 
-function sendMessage() {
-    var msg = document.getElementById('chatbox').value;
+function appendMessage(payload) {
+    var text = payload['username'] + ': ' + payload['message'];
+    $('#messages').append($('<li>').text(text));
+}
 
-    var payload = {
-        'username': username,
-        'message': msg
-    }
 
-    socket.emit('chatmsg', payload);
-    document.getElementById('chatbox').value = "";
+function buildTypingListElement(username) {
+    var id = getTypingId(username);
+    return `<li id=${id}>${username} is typing...</id>`;
+}
+
+function getTypingId(username) {
+    return `${username}-typing`;
 }
